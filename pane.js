92f76175f1c28c42c3e151ac28a8fb206e7e0c58
joeyoji <hirose.j.aa@m.titech.ac.jp>
2021-07-01 18:35:08 +0900

    header fixed

--- pane.js
+++ pane.js
@@ -2,14 +2,13 @@ function header(){
 	document.write('\
 	<header>\
 	<p><a href="index.html">ホーム</a></p>\
+	<hr>\
 	</header>\
 	');
 }
-
 function footer(){
 	document.write('\
 	<footer>\
-	<p><a href="index.html">ホーム</a></p>\
 	<hr>\
 	<div class="foot_note">\
 	<p>※現在pdfの埋め込みについてはgoogle documentを採用していますが<br>\
