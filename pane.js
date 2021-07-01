7439bdfaeacf4eee1f30762f8be0ca0d05bdb6a5
joeyoji <hirose.j.aa@m.titech.ac.jp>
2021-07-01 22:16:53 +0900

    page delte

--- pane.js
+++ pane.js
@@ -11,9 +11,6 @@ function footer(){
 	<footer>\
 	<hr>\
 	<div class="foot_note">\
-	<p>※現在pdfの埋め込みについてはgoogle documentを採用していますが<br>\
-	読み込みが出来ない場合があります.\
-	そのときは何回かページを更新してみてください.</p>\
 	</div>\
 	</footer>\
 	');
